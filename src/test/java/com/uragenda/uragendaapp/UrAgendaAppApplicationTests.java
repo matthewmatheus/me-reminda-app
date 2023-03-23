048e8dc26a9e6052d215434ce0222e6433dd527b
Matt <mattmoura.jvm@gmail.com>
2023-03-22 21:14:27 -0300

    first commit

---
+++ src/test/java/com/uragenda/uragendaapp/UrAgendaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uragenda.uragendaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrAgendaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
